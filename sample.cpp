19a20b168d0c669a11f2cbf8c935ad7364b88c0d
Konstantin Erman <kerman@tableau.com>
2020-11-03 16:27:38 -0800

    first commit

---
+++ sample.cpp
@@ -0,0 +1,31 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+std::string get_greet(const std::string& who) {
+  return "Hello " + who;
+}
+
+void print_localtime() {
+  std::time_t result = std::time(nullptr);
+  std::cout << std::asctime(std::localtime(&result));
+}
+
+int main(int argc, char** argv) {
+  std::string who = "world";
+  if (argc > 1) {
+    who = argv[1];
+  }
+  std::cout << get_greet(who) << std::endl;
+  print_localtime();
+
+  Sleep(70000); // Milliseconds
+
+  return 0;
+}
